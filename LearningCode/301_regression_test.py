fed039a88e0f6e936a9bb6131cab2d26745a2142
zeroLR <ceyanglab@gmail.com>
2020-02-05 20:00:51 +0800

    add 301_regression_test.py

---
+++ LearningCode/301_regression_test.py
@@ -0,0 +1,55 @@
+"""
+Dependencies:
+torch: 1.4(Stable)
+numpy
+matplotlib
+"""
+import torch
+import torch.nn.functional as F
+import matplotlib.pylab as plt
+
+x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)  # x data (tensor) , shape(200, 1)
+y = x.pow(2) + 0.2*torch.rand(x.size()) # noisy y data (tensor) , shape(200, 1)
+
+class Net(torch.nn.Module):
+    def __init__(self, n_feature, n_hidden, n_output):
+        super(Net, self).__init__()
+        self.hidden = torch.nn.Linear(n_feature, n_hidden)  # hidden layer
+        self.predict = torch.nn.Linear(n_hidden, n_output)  # output layer
+
+    def forward(self, x):
+        x = F.relu(self.hidden(x))      # activation funtion for hidden layer
+        x = self.predict(x)             # linear output
+        return x
+
+net = Net(n_feature=1, n_hidden=10, n_output=1)     # define the network
+print(net)
+
+optimizer = torch.optim.SGD(net.parameters(), lr=0.15, momentum=0.9, nesterov=True)
+loss_func = torch.nn.MSELoss()  # this is for regression mean squared loss
+
+plt.ion()   # something about plotting
+
+for t in range(200):
+    prediction = net(x)     # input x and predict based on x
+
+    loss = loss_func(prediction, y)     # must be (nn output, target)
+
+    optimizer.zero_grad()   # clear gradients for next train
+    loss.backward()         # backpropagation, compute gradients
+    optimizer.step()        # apply gradients
+
+    if t % 5 == 0:
+        # plot and show learning process
+        plt.cla()
+        plt.scatter(x.data.numpy(), y.data.numpy())
+        plt.plot(x.data.numpy(), prediction.data.numpy(), 'r-', lw=5)
+        plt.text(0.5, 0, 'Loss=%.4f' % loss.data.numpy(), fontdict={'size': 20, 'color': 'red'})
+        plt.pause(0.1)
+
+        
+
+
+
+plt.ioff()
+plt.show()
